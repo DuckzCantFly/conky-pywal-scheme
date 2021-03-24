a0e68ff0b3e4850e7a4f3f0b85ab765d2ae6c49e
Duckz@protonmail.ch <Duckz@protonmail.ch>
2021-03-24 14:00:14 -0600

    adds propper cmd to pywal postrun

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 sudo cp -f ./cpws-gc ./cpws-reset ./cpws-rlist /usr/local/bin/
 
 grep '#conky color scheme start
-cok-pyw-sh --reset-color
+cpws-reset --reset-color
 #conky color scheme end' $XDG_CONFIG_HOME/wal/postrun > /dev/null|| echo '
 #conky color scheme start
-cok-pyw-sh --reset-color
+cpws-reset --reset-color
 #conky color scheme end' >> $XDG_CONFIG_HOME/wal/postrun
