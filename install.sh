b52a975553c5866d056b1ea8ef77458d413abf36
Duckz@protonmail.ch <Duckz@protonmail.ch>
2021-04-28 22:48:11 -0600

    slightly better help (not good enough) & clean up

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo cp -f ./cpws-gc ./cpws-reset ./cpws-rlist /usr/local/bin/
 
 grep '#conky color scheme start
 cpws-reset --reset-color
-#conky color scheme end' $XDG_CONFIG_HOME/wal/postrun > /dev/null || echo '
+#conky color scheme end' "$XDG_CONFIG_HOME/wal/postrun" > /dev/null || echo '
 #conky color scheme start
 cpws-reset --reset-color
-#conky color scheme end' >> $XDG_CONFIG_HOME/wal/postrun
+#conky color scheme end' >> "$XDG_CONFIG_HOME/wal/postrun"
